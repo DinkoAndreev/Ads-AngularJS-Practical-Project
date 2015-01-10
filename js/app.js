771c7d966a0bd4ccf1abf1ef0bf4ab42c36ebf90
DinkoAndreev <dinkoandreev@gmail.com>
2015-01-10 09:26:22 +0200

    new stuff

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 'use strict';
 
-var app = angular.module('SUAds', ['ngResourse']);
+var app = angular.module('SUAds', ['ngRoute','ngResourse']);
 
+SUAds.config(function($routeProvider){
+    $routeProvider
+        .when('', {
+            templateUrl: '',
+            controller: ''
+    })
+        .otherwise({redirectTo: ''})
+})
